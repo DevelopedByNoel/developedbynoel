5220d0ab8b19630379d56f250eacf6406cf0ee7b
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-11-22 19:15:32 +0000

    completed draft navbar - will require updates to links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import {
     FaBars,
     FaTimes,
@@ -7,9 +7,8 @@ import {
     FaInstagram,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPeronLinesFill } from 'react-icons/bs';
 import { Link } from 'react-scroll';
- 
+
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
@@ -20,42 +19,99 @@ const Navbar = () => {
             <div>
                 <h1 className="font-thin text-2xl italic font-serif">NG</h1>
             </div>
-    {/* Menu */}
-    <ul className="hidden md:flex gap-x-8">
-        <li>
-            <Link to='home' smooth={true} duration={500} >
-            Home
-            </Link>
-        </li>
-        <li>
-            <Link to='about' smooth={true} duration={500} >
-            About
-            </Link>
-            </li>
-            <li>
-            <Link to='skills' smooth={true} duration={500} >
-            Skills
-            </Link>
-        </li>
-        <li>
-            <Link to='mywork' smooth={true} duration={500} >
-            My Work
-            </Link>
-        </li>
-        <li>
-            <Link to='contact' smooth={true} duration={500} >
-            Contact
-            </Link>
-        </li>
-    </ul>
-    <div onClick={handleClick} className="md:hidden z-10">
-        {!nav ? <FaBars /> : <FaTimes />}
-    </div>
+            {/* Menu */}
+            <ul className="hidden md:flex gap-x-8">
+                <li>
+                    <Link to='home' smooth={true} duration={500} >
+                        Home
+                    </Link>
+                </li>
+                <li>
+                    <Link to='about' smooth={true} duration={500} >
+                        About
+                    </Link>
+                </li>
+                <li>
+                    <Link to='skills' smooth={true} duration={500} >
+                        Skills
+                    </Link>
+                </li>
+                <li>
+                    <Link to='mywork' smooth={true} duration={500} >
+                        My Work
+                    </Link>
+                </li>
+                <li>
+                    <Link to='contact' smooth={true} duration={500} >
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+            <div onClick={handleClick} className="md:hidden z-10">
+                {!nav ? <FaBars /> : <FaTimes />}
+            </div>
 
-{/* This is the menu for mobile */}
+            {/* This is the menu for mobile */}
+            <ul className={
+                !nav
+                    ? 'hidden'
+                    : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+            }>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+                        Home
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+                        About
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+                        Skills
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to='mywork' smooth={true} duration={500}>
+                        My Work
+                    </Link>
+                </li>
+                <li className="py-6 text-4xl">
+                    <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+
+            <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+                <ul>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-100px] duration-300 bg-blue-600">
+                        <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                            Linkedin <FaLinkedin size={30} />
+                        </a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-100px] duration-300 bg-blue-600">
+                        <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                            Github <FaGithub size={30} />
+                        </a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-100px] duration-300 bg-blue-600">
+                        <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                            Instagram <FaInstagram size={30} />
+                        </a>
+                    </li>
+                    <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-100px] duration-300 bg-blue-600">
+                        <a className="flex justify-between items-center w-full text-gray-300" href="/">
+                            Email <HiOutlineMail size={30} />
+                        </a>
+                    </li>
+
+                </ul>
+            </div>
+
         </div>
     )
 }
-
 
 export default Navbar;
