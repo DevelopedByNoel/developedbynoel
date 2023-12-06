d2331097b56c00809f9733d40155653797ff6c88
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-12-06 16:27:24 +0000

    contact section completed, need to add link to email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 
-function Contact() {
+const Contact = () => {
     return (
-        <div></div>
+        <div name='contact' className="w-full h-screen bg-[#0a192f] flex justify-center items-center p-4">
+            <div className="flex flex-col max-w-[600px] w-full">
+                <div className="pb-8 flex flex-col justify-center w-full h-full items-center">
+                    <p className="text-4xl font-bold inline border-b-4 border-cyan-500 text-gray-300">Contact Me</p>
+                    <p className="text-gray-300 py-4">Have you got a project in mind that you need some help with? Send me a message and we can discuss your idea further and bring it to life!</p>
+                </div>
+                    <input className="bg-[#ccd6f6] p-2" type="text" placeholder="Name" name='name' />
+                    <input className="my-4 bg-[#ccd6f6] p-2" type="text" placeholder="Email" name='email' />
+                    <textarea className="bg-[#ccd6f6] p-2" name='message' rows="10" placeholder="Message"></textarea>
+                    <button className="text-white border-2 hover:bg-cyan-500 hover:border-cyan-500 px-6 py-2 my-4 mx-auto flex items-center">Send</button>
+            </div>
+        </div>
     )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -5,7 +5,7 @@ const Skills = () => {
         <div name='skills' className="w-full h-screen bg-[#0a192f] text-gray-300">
             <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
                 <p className="text-4xl font-bold inline border-b-4 border-cyan-500 text-center">Skills</p>
-                <p className="py-4 text-2xl">I am always looking to improve myself and learn new things. Here is a list of technologies I have used so far</p>
+                <p className="py-4 text-2xl text-center">I am always looking to improve myself and learn new things. Here is a list of technologies I have used so far:</p>
 
 
                 <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
