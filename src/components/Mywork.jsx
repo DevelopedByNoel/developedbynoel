3f87d30bd4f9b103b0ce6b986d32467797df649c
ngalvin1991 <117473760+ngalvin1991@users.noreply.github.com>
2023-12-06 15:42:07 +0000

    completed skills sections and started on project section

--- src/components/Mywork.jsx
+++ src/components/Mywork.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
-function Mywork() {
+const Mywork = () => {
     return (
-        <div></div>
+        <div name='work' className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <div className="pb-8 w-full flex justify-center items-center flex-col">
+                    <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500">My Work</p>
+                    <p className="py-6 text-2xl">Here are some of the projects that I have worked on.</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 
-function Skills() {
+const Skills = () => {
     return (
-        <div></div>
+        <div name='skills' className="w-full h-screen bg-[#0a192f] text-gray-300">
+            <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+                <p className="text-4xl font-bold inline border-b-4 border-cyan-500 text-center">Skills</p>
+                <p className="py-4 text-2xl">I am always looking to improve myself and learn new things. Here is a list of technologies I have used so far</p>
+
+
+                <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">HTML</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">CSS</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">Javascript</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">React</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">Next.js</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">Tailwind CSS</p>
+                    </div>
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">Node.js</p>
+                    </div>
+
+                    <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-500">
+                        <p className="my-4">MongoDB</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
     )
 }
 
